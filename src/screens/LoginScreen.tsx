43253ae2ed548c69e378a7472043694dc3096610
Robertino <robertino.dimoski@outlook.com>
2022-10-30 01:40:24 +0200

    InitialCommit

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,17 @@
+import { View, Text,StyleSheet } from 'react-native'
+import React from 'react'
+export default function LoginScreen() {
+  return (
+    <View style={styles.container}>
+            <Text>Test</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
